ba48cfdb131437bd21a558946cda0aaf5ba9d51f
Nguyễn Nhĩ Thái <62824044+nhithai4620@users.noreply.github.com>
2021-10-03 08:22:16 +0700

    thai commit at 8:22 10/3/2021

--- backend/index.js
+++ backend/index.js
@@ -1,42 +1,21 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.use(function (req, res, next) {
+var server = require("http").Server(app);
+var io = require("socket.io")(server);
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
-
-app.use(bodyParser.json());
-
-app.use(bodyParser.urlencoded({ extended: true })); 
-
-app.post("/api/submit", (req, res) => {
-    var student = req.body;
-    if (student){
-        console.log(student);
-        res.send("success");
-    }
-    else {
-        res.send("false")
-    }
-    
-})
-
-app.listen(5000,function(){
+server.listen(5000,function(){
     console.log('Node server runing @http://localhost:5000');
 })
+
+io.on("connection",function(socket){
+    console.log("co nguoi ket noi");
+});
+
+app.get("/", function(req,res){
+    res.render("trangchu");
+})
+
